aa0a2be1a3f060f386734cb1924db32d9712cc72
Carlos Doki <carlosdoki@Carloss-MacBook-Pro.local>
2017-03-20 18:04:45 -0300

    Sign in ui custom classes complete

---
+++ Doki-Social/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Doki-Social
+//
+//  Created by Carlos Doki on 20/03/17.
+//  Copyright © 2017 Carlos Doki. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
---
+++ Doki-Social/FancyField.swift
@@ -0,0 +1,28 @@
+//
+//  FancyField.swift
+//  Doki-Social
+//
+//  Created by Carlos Doki on 20/03/17.
+//  Copyright © 2017 Carlos Doki. All rights reserved.
+//
+
+import UIKit
+
+class FancyField: UITextField {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        layer.borderColor = UIColor(red: SHADOW_GRAY, green: SHADOW_GRAY, blue: SHADOW_GRAY, alpha: 0.2).cgColor
+        layer.borderWidth = 1.0
+        layer.cornerRadius = 2.0
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+    
+    override func editingRect(forBounds bounds: CGRect) -> CGRect {
+        return bounds.insetBy(dx: 10, dy: 5)
+    }
+}
