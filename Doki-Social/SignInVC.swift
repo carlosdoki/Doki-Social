ad1bc17592a41f08014d580964281a54b4b20c6b
Carlos Doki <carlosdoki@Doki-Econocom-MacBookPro.local>
2017-03-21 13:34:00 -0300

    Finish Pod Facebbok Login

--- Doki-Social/SignInVC.swift
+++ Doki-Social/SignInVC.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
+import Firebase
 
 class SignInVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    @IBAction func facebookBtnTapped(_ sender: AnyObject) {
+        let facebookLogin = FBSDKLoginManager()
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("DOKI: Unable to authenticate with Facebook - \(error)")
+            } else if result?.isCancelled == true {
+                print("DOKI: User cancelled Facebook authentication")
+            } else {
+                print("DOKI: Sucessfully authentication with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("DOKI: unable to authentication with Firebase - \(error)")
+            } else {
+                print("DOKI: Sucessfully authentication with Firebase")
+            }
+        })
     }
 }
 
--- Podfile
+++ Podfile
@@ -10,4 +10,5 @@ target 'Doki-Social' do
   pod 'Firebase/Auth'
   pod 'Firebase/Database'
   pod 'Firebase/Storage'
+  pod 'FBSDKLoginKit'
 end
