4228130b80a0324802895ddcfe1f384664b61826
Carlos Doki <carlosdoki@Carloss-MacBook-Pro.local>
2017-03-23 08:47:31 -0300

    Finish firebase parse

--- Doki-Social/FeedVC.swift
+++ Doki-Social/FeedVC.swift
@@ -55,8 +55,12 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let post = posts[indexPath.row]
-        print("Doki: post=\(post.caption)")
-        return (tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell)!
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell {
+            cell.configureCell(post: post)
+            return cell
+        } else {
+            return PostCell()
+        }
     }
 
     @IBAction func singoutBtnPressed(_ sender: UIButton) {
