888d0ca9a122a49a96c9a6b93480c3dfc0303438
Carlos Doki <carlosdoki@Carloss-MacBook-Pro.local>
2017-03-23 08:37:56 -0300

    Finish firebase get

--- Doki-Social/FeedVC.swift
+++ Doki-Social/FeedVC.swift
@@ -17,6 +17,8 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     @IBOutlet weak var bannerView: GADBannerView!
     @IBOutlet weak var tableView: UITableView!
     
+    var posts = [Post]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,7 +26,17 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         tableView.dataSource = self
         
         DataService.ds.REF_POSTS.observe(.value, with: { (snapshot) in
-            print("DOKI: snapshot = \(snapshot.value)")
+            if let snapshot = snapshot.children.allObjects as? [FIRDataSnapshot] {
+                for snap in snapshot {
+                    print("DOKI: \(snap)")
+                    if let postDict = snap.value as? Dictionary<String, AnyObject> {
+                        let key = snap.key
+                        let post = Post(postKey: key, postData: postDict)
+                        self.posts.append(post)
+                    }
+                }
+            }
+            self.tableView.reloadData()
         })
         
 //        print("Google Mobile Ads SDK version: " + GADRequest.sdkVersion())
@@ -38,10 +50,12 @@ class FeedVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return posts.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let post = posts[indexPath.row]
+        print("Doki: post=\(post.caption)")
         return (tableView.dequeueReusableCell(withIdentifier: "PostCell") as? PostCell)!
     }
 
